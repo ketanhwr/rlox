992cf0d33cbcc33ad89b0b3d6a2957ff2b313669
Ketan Gupta <ketan@cloudflare.com>
2025-11-04 18:13:29 +0530

    Line numbers

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ enum OpCode {
 
 struct Chunk {
     code: Vec<OpCode>,
+    lines: Vec<usize>,
     constants: Vec<f64>,
 }
 
 impl Chunk {
-    fn write_chunk(&mut self, opcode: OpCode) {
+    fn write_chunk(&mut self, opcode: OpCode, line: usize) {
         self.code.push(opcode);
+        self.lines.push(line)
     }
 
     fn add_constant(&mut self, value: f64) -> usize {
@@ -23,13 +25,13 @@ impl Chunk {
 fn main() {
     let mut chunk = Chunk {
         code: Vec::new(),
+        lines: Vec::new(),
         constants: Vec::new(),
     };
 
-    chunk.write_chunk(OpCode::OpReturn);
-
     let constant = OpCode::OpConstant(chunk.add_constant(1.2));
-    chunk.write_chunk(constant);
+    chunk.write_chunk(constant, 123);
+    chunk.write_chunk(OpCode::OpReturn, 123);
 
     disassemble_chunk(&chunk, "test chunk");
 }
@@ -37,7 +39,16 @@ fn main() {
 fn disassemble_chunk(chunk: &Chunk, name: &str) {
     println!("=== {name} ===");
 
-    for (i, instr) in chunk.code.iter().enumerate() {
-        println!("{i:04} {instr:?}");
+    for i in 0..chunk.code.len() {
+        print!("{i:04} ");
+        if i > 0 && chunk.lines[i] == chunk.lines[i - 1] {
+            print!("   | ");
+        } else {
+            print!("{:4} ", chunk.lines[i]);
+        }
+        match chunk.code[i] {
+            OpCode::OpReturn => println!("{:?}", chunk.code[i]),
+            OpCode::OpConstant(idx) => println!("{:?} {}", chunk.code[i], chunk.constants[idx]),
+        }
     }
 }
